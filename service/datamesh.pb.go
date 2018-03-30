3f5621b4c586e7525908b740a23fd1500044115d
nferraro <ni.ferraro@gmail.com>
2018-03-31 01:15:57 +0200

    Add observers

--- service/datamesh.pb.go
+++ service/datamesh.pb.go
@@ -8,6 +8,7 @@ It is generated from these files:
 	service/datamesh.proto
 
 It has these top-level messages:
+	ObserverStatus
 	Event
 	Empty
 */
@@ -33,6 +34,22 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
+type ObserverStatus struct {
+	Version int64 `protobuf:"varint,1,opt,name=version" json:"version,omitempty"`
+}
+
+func (m *ObserverStatus) Reset()                    { *m = ObserverStatus{} }
+func (m *ObserverStatus) String() string            { return proto.CompactTextString(m) }
+func (*ObserverStatus) ProtoMessage()               {}
+func (*ObserverStatus) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+
+func (m *ObserverStatus) GetVersion() int64 {
+	if m != nil {
+		return m.Version
+	}
+	return 0
+}
+
 type Event struct {
 	Name    string `protobuf:"bytes,1,opt,name=name" json:"name,omitempty"`
 	Payload []byte `protobuf:"bytes,2,opt,name=payload,proto3" json:"payload,omitempty"`
@@ -41,7 +58,7 @@ type Event struct {
 func (m *Event) Reset()                    { *m = Event{} }
 func (m *Event) String() string            { return proto.CompactTextString(m) }
 func (*Event) ProtoMessage()               {}
-func (*Event) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{0} }
+func (*Event) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
 
 func (m *Event) GetName() string {
 	if m != nil {
@@ -63,9 +80,10 @@ type Empty struct {
 func (m *Empty) Reset()                    { *m = Empty{} }
 func (m *Empty) String() string            { return proto.CompactTextString(m) }
 func (*Empty) ProtoMessage()               {}
-func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{1} }
+func (*Empty) Descriptor() ([]byte, []int) { return fileDescriptor0, []int{2} }
 
 func init() {
+	proto.RegisterType((*ObserverStatus)(nil), "service.ObserverStatus")
 	proto.RegisterType((*Event)(nil), "service.Event")
 	proto.RegisterType((*Empty)(nil), "service.Empty")
 }
@@ -82,6 +100,7 @@ const _ = grpc.SupportPackageIsVersion4
 
 type DataMeshClient interface {
 	Push(ctx context.Context, in *Event, opts ...grpc.CallOption) (*Empty, error)
+	Observe(ctx context.Context, in *ObserverStatus, opts ...grpc.CallOption) (DataMesh_ObserveClient, error)
 }
 
 type dataMeshClient struct {
@@ -101,10 +120,43 @@ func (c *dataMeshClient) Push(ctx context.Context, in *Event, opts ...grpc.CallO
 	return out, nil
 }
 
+func (c *dataMeshClient) Observe(ctx context.Context, in *ObserverStatus, opts ...grpc.CallOption) (DataMesh_ObserveClient, error) {
+	stream, err := grpc.NewClientStream(ctx, &_DataMesh_serviceDesc.Streams[0], c.cc, "/service.DataMesh/Observe", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &dataMeshObserveClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type DataMesh_ObserveClient interface {
+	Recv() (*Event, error)
+	grpc.ClientStream
+}
+
+type dataMeshObserveClient struct {
+	grpc.ClientStream
+}
+
+func (x *dataMeshObserveClient) Recv() (*Event, error) {
+	m := new(Event)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // Server API for DataMesh service
 
 type DataMeshServer interface {
 	Push(context.Context, *Event) (*Empty, error)
+	Observe(*ObserverStatus, DataMesh_ObserveServer) error
 }
 
 func RegisterDataMeshServer(s *grpc.Server, srv DataMeshServer) {
@@ -129,6 +181,27 @@ func _DataMesh_Push_Handler(srv interface{}, ctx context.Context, dec func(inter
 	return interceptor(ctx, in, info, handler)
 }
 
+func _DataMesh_Observe_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(ObserverStatus)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(DataMeshServer).Observe(m, &dataMeshObserveServer{stream})
+}
+
+type DataMesh_ObserveServer interface {
+	Send(*Event) error
+	grpc.ServerStream
+}
+
+type dataMeshObserveServer struct {
+	grpc.ServerStream
+}
+
+func (x *dataMeshObserveServer) Send(m *Event) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 var _DataMesh_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "service.DataMesh",
 	HandlerType: (*DataMeshServer)(nil),
@@ -138,22 +211,31 @@ var _DataMesh_serviceDesc = grpc.ServiceDesc{
 			Handler:    _DataMesh_Push_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "Observe",
+			Handler:       _DataMesh_Observe_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "service/datamesh.proto",
 }
 
 func init() { proto.RegisterFile("service/datamesh.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 147 bytes of a gzipped FileDescriptorProto
+	// 197 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2b, 0x4e, 0x2d, 0x2a,
 	0xcb, 0x4c, 0x4e, 0xd5, 0x4f, 0x49, 0x2c, 0x49, 0xcc, 0x4d, 0x2d, 0xce, 0xd0, 0x2b, 0x28, 0xca,
-	0x2f, 0xc9, 0x17, 0x62, 0x87, 0x8a, 0x2b, 0x99, 0x72, 0xb1, 0xba, 0x96, 0xa5, 0xe6, 0x95, 0x08,
-	0x09, 0x71, 0xb1, 0xe4, 0x25, 0xe6, 0xa6, 0x4a, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0x81, 0xd9,
-	0x42, 0x12, 0x5c, 0xec, 0x05, 0x89, 0x95, 0x39, 0xf9, 0x89, 0x29, 0x12, 0x4c, 0x0a, 0x8c, 0x1a,
-	0x3c, 0x41, 0x30, 0xae, 0x12, 0x3b, 0x17, 0xab, 0x6b, 0x6e, 0x41, 0x49, 0xa5, 0x91, 0x09, 0x17,
-	0x87, 0x4b, 0x62, 0x49, 0xa2, 0x6f, 0x6a, 0x71, 0x86, 0x90, 0x06, 0x17, 0x4b, 0x40, 0x69, 0x71,
-	0x86, 0x10, 0x9f, 0x1e, 0xd4, 0x74, 0x3d, 0xb0, 0xd1, 0x52, 0x48, 0x7c, 0x90, 0x1e, 0x25, 0x86,
-	0x24, 0x36, 0xb0, 0x2b, 0x8c, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0xf1, 0x8f, 0xdc, 0x24, 0x9f,
-	0x00, 0x00, 0x00,
+	0x2f, 0xc9, 0x17, 0x62, 0x87, 0x8a, 0x2b, 0x69, 0x71, 0xf1, 0xf9, 0x27, 0x81, 0x38, 0xa9, 0x45,
+	0xc1, 0x25, 0x89, 0x25, 0xa5, 0xc5, 0x42, 0x12, 0x5c, 0xec, 0x65, 0xa9, 0x45, 0xc5, 0x99, 0xf9,
+	0x79, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0xcc, 0x41, 0x30, 0xae, 0x92, 0x29, 0x17, 0xab, 0x6b, 0x59,
+	0x6a, 0x5e, 0x89, 0x90, 0x10, 0x17, 0x4b, 0x5e, 0x62, 0x6e, 0x2a, 0x58, 0x9e, 0x33, 0x08, 0xcc,
+	0x06, 0x69, 0x2b, 0x48, 0xac, 0xcc, 0xc9, 0x4f, 0x4c, 0x91, 0x60, 0x52, 0x60, 0xd4, 0xe0, 0x09,
+	0x82, 0x71, 0x95, 0xd8, 0xb9, 0x58, 0x5d, 0x73, 0x0b, 0x4a, 0x2a, 0x8d, 0x72, 0xb8, 0x38, 0x5c,
+	0x12, 0x4b, 0x12, 0x7d, 0x53, 0x8b, 0x33, 0x84, 0x34, 0xb8, 0x58, 0x02, 0x4a, 0x8b, 0x33, 0x84,
+	0xf8, 0xf4, 0xa0, 0x2e, 0xd1, 0x03, 0x1b, 0x2d, 0x85, 0xc4, 0x07, 0xe9, 0x51, 0x62, 0x10, 0x32,
+	0xe3, 0x62, 0x87, 0xba, 0x50, 0x48, 0x1c, 0x2e, 0x89, 0xea, 0x66, 0x29, 0x34, 0x53, 0x94, 0x18,
+	0x0c, 0x18, 0x93, 0xd8, 0xc0, 0x3e, 0x35, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x27, 0x07, 0x98,
+	0x86, 0x03, 0x01, 0x00, 0x00,
 }
